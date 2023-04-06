6b0ff6558caa53f25ccf8ab40f702b82c06d0433
Julia Morgunova <julia.ivanova.mars@gmail.com>
2023-04-06 17:51:42 +0600

    fix project structure (#3)
    * fix project structure
    
    * add gitignore
    
    * fix gitignore
    
    ---------
    
    Co-authored-by: Julia Morgunova <julia.ivanova.mars@gmail,com>

--- src/main/java/com/epam/training/student_Iuliia_Morgunova/Module_1/1. Meet AutoCode/HelloAutocode.java
+++
@@ -1,8 +0,0 @@
-package com.epam.training.student_Iuliia_Morgunova.Module_1;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        //Write a program, printing "Hello, Autocode!"
-        System.out.println("Hello, Autocode!");
-    }
-}
