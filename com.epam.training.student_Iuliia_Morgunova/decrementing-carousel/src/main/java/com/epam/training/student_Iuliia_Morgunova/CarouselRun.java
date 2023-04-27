6a1fa4b23f292e6c228e3515359b939787b407bf
Julia Morgunova <julia.ivanova.mars@gmail,com>
2023-04-27 12:46:28 +0600

    solved Decrementing Carousel

---
+++ com.epam.training.student_Iuliia_Morgunova/decrementing-carousel/src/main/java/com/epam/training/student_Iuliia_Morgunova/CarouselRun.java
@@ -0,0 +1,37 @@
+package com.epam.training.student_Iuliia_Morgunova;
+
+public class CarouselRun
+{
+    private int index = 0;          // index of a current element
+
+    public int next() {
+        if (this.isFinished()) return -1;
+
+        while (DecrementingCarousel.carousel[index] == 0){                    // while current value == 0
+            if (index < DecrementingCarousel.carousel.length-1){
+                index++;                                                      // go to the next value
+            } else if (index == DecrementingCarousel.carousel.length-1) {
+                index = 0;
+            }
+        }
+
+        int value = DecrementingCarousel.carousel[index];         // saving a value of a current element
+        DecrementingCarousel.carousel[index]--;
+
+        if (index < DecrementingCarousel.carousel.length-1){
+            index++;
+        } else if (index == DecrementingCarousel.carousel.length-1) {
+            index = 0;
+        }
+
+        return value;
+    }
+
+    public boolean isFinished() {
+        int positiveElements = 0;                       // number of positive elements in the array (value > 0)
+        for (int el: DecrementingCarousel.carousel) {
+            if (el > 0) positiveElements++;
+        }
+        return positiveElements == 0;
+    }
+}
